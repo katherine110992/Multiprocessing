fc1eb82276bec9d8500ac5b0ed3719043135abfa
Katherine Espindola <yespindola@javeriana.edu.co>
2017-11-07 15:38:06 -0500

    Second commit. parallel_run.py added with some improvements

---
+++ logic/parallel_run.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process, Queue
+
+
+def create_graph(number, vertex, nodes, queue):
+    queue.put([number, vertex * nodes])
+
+if __name__ == '__main__':
+    queue = Queue()
+
+    vertex_1 = 20
+    vertex_2 = 30
+    nodes_1 = 10
+    nodes_2 = 30
+
+    graph_process_1 = Process(target=create_graph, args=(1, vertex_1, nodes_1, queue))
+    graph_process_2 = Process(target=create_graph, args=(2, vertex_2, nodes_2, queue))
+
+    graph_process_1.start()
+    graph_process_2.start()
+
+    graph_process_1.join()
+    graph_process_2.join()
+
+    while not queue.empty():
+        print(queue.get())
--- logic/run.py
+++ logic/run.py
@@ -1,29 +1,47 @@
-import multiprocessing
+from multiprocessing import Process, Queue
+import difflib, random, time
 
 
-def create_graph(vertex, nodes):
-    return list(range(vertex * nodes))
+def f2(wordlist, mainwordlist, q):
+    for mainword in mainwordlist:
+        matches = difflib.get_close_matches(mainword,wordlist,len(wordlist),0.7)
+        q.put(matches)
 
 
-def start_process():
-    print('Starting', multiprocessing.current_process().name)
+def splitlist(inlist, chunksize):
+    return [inlist[x:x + int(chunksize)] for x in range(0, int(len(inlist)), int(chunksize))]
 
 if __name__ == '__main__':
-    vertex_1 = 20
-    vertex_2 = 30
-    nodes_1 = 10
-    nodes_2 = 30
 
-    inputs = list()
-    inputs.append([vertex_1, nodes_1])
-    inputs.append([vertex_2, nodes_2])
+    # constants (for 50 input words, find closest match in list of 100 000 comparison words)
+    q = Queue()
+    wordlist = ["".join([random.choice([letter for letter in "abcdefghijklmnopqersty"]) for lengthofword in range(5)]) for nrofwords in range(100000)]
+    mainword = "hello"
+    mainwordlist = [mainword for each in range(50)]
 
-    pool = multiprocessing.Pool(processes=2, initializer=start_process,)
-    pool_outputs = pool.starmap(create_graph, inputs)
-    pool.close()  # no more tasks
-    pool.join()  # wrap up current tasks
+    # normal approach
+    t = time.time()
+    for mainword in mainwordlist:
+        matches = difflib.get_close_matches(mainword,wordlist,len(wordlist),0.7)
+        q.put(matches)
+    print(time.time()-t)
 
-    graph_1 = pool_outputs[0]
-    graph_2 = pool_outputs[1]
-    print(graph_1)
-    print(graph_2)
+    # split work into 5 or 10 processes
+    processes = 5
+    print(int(len(mainwordlist))/processes)
+    mainwordlistsplitted = splitlist(mainwordlist, int(len(mainwordlist))/processes)
+    print("list ready")
+
+    t = time.time()
+    all_processes = list()
+    for submainwordlist in mainwordlistsplitted:
+        print("sub")
+        p = Process(target=f2, args=(wordlist,submainwordlist,q,))
+        p.Daemon = True
+        all_processes.append(p)
+        p.start()
+    for p in all_processes:
+        p.join()
+    print(time.time()-t)
+    while not q.empty():
+        print(q.get())
